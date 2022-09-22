2f4f06ba5358f8194bd2d405f4f755cca6c337ed
muddassir1112 <muddassiransari2022aug@cedcoss.com>
2022-09-22 17:57:47 +0530

    UI Improved

--- src/script.js
+++ src/script.js
@@ -1,12 +1,12 @@
 
 function button(param){
-    document.getElementById('input').value += param;
+    document.getElementById('input').value += param; //Taken input from input box and concatenating it
 }
 
-function input(){
+function input(){ // This function helps to calculate the resultant value 
     var result = [];
     if(document.getElementById('input').value.includes("-")){
-        result = document.getElementById('input').value.split('-');
+        result = document.getElementById('input').value.split('-'); // Split function breaks an array into two parts as per the given instructions
         return parseInt(result[0] - result[1]);
     }
     else if(document.getElementById('input').value.includes("+")){
@@ -22,11 +22,11 @@ function input(){
         return parseInt(result[0] * result[1]);
     }
 }
-function output(){
+function output(){ // The resultant value will get stored in the variable and will dispaly in the input box
     var ans = input();
     document.getElementById('input').value = ans ;
 }
-function reset(){
+function reset(){ // This function helps to clear the values from the input box
     document.getElementById('input').value = "";
 }
  
